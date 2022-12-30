37e5901ca42756821db338f4a3f91807745eb6c8
Android-Piepaint <73344795+Android-Piepaint@users.noreply.github.com>
2022-12-30 14:49:38 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,5 @@
 // support for .env, .env.development, and .env.production
-require("dotenv").config()
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+
 
 module.exports = {
   siteMetadata: {
@@ -13,12 +10,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-drupal`,
-      options: {
-        baseUrl: process.env.DRUPAL_BASE_URL,
-        basicAuth: {
-          username: process.env.DRUPAL_BASIC_AUTH_USERNAME,
-          password: process.env.DRUPAL_BASIC_AUTH_PASSWORD,
         },
         fastBuilds: true,
       },
